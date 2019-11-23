1ea844a3c3cbbb6e2833b9b7d88a04ffd4ce7eff
Timofei <kiselev7788@gmail.com>
2019-11-24 01:26:23 +0300

    .

---
+++ Palindrom.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool IsPaalindrom(string w) {
+	cin >> w;
+	for (i = 0; i < w.size() / 2; ++i) {
+		if (w[i] != w[s.size() - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
