1afffeb256b8ffd5cdc7857eb25d71bc6d9943c7
Timofei <43321943+Kiselya@users.noreply.github.com>
2019-11-24 02:02:29 +0300

    Update Palindrom.cpp

--- Palindrom.cpp
+++ Palindrom.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool IsPaalindrom(string w) {
 
 	for (int i = 0; i < w.size() / 2; ++i) {
-		if (w[i] != w[s.size() - i - 1]) {
+		if (w[i] != w[w.size() - i - 1]) {
 			return false;
 		}
 	}
